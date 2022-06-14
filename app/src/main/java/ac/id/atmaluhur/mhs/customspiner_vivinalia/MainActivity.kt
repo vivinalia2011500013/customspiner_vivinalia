d4042195062234ac8498b4fe82f0525822feef71
Pratiwi31 <ratiwi31@gmail.com>
2022-06-14 17:33:04 +0700

    Initial commit

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/customspiner_vivinalia/MainActivity.kt
@@ -0,0 +1,26 @@
+package ac.id.atmaluhur.mhs.customspiner_vivinalia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    val nama = arrayOf(
+        "Kota Pangkalpinang", "Kabupaten Bangka", "Kabupaten Bangka Barat",
+        "Kabupaten Bangka Tengah", "Kabupaten Bangka Selatan", "Kabupaten Belitung",
+        "Kabuapaten Belitung Timur"
+    )
+    val logo = intArrayOf(
+        R.drawable.logo_pangkalpinang, R.drawable.logo_bangka, R.drawable.logo_babar,
+        R.drawable.logo_bateng, R.drawable.logo_basel, R.drawable.logo_belitung,
+        R.drawable.logo_beltim
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spnKabKota = findViewById<Spinner>(R.id.spnKabKota)
+        val adpKabKota = KabKota(this@MainActivity, logo, nama)
+        spnKabKota.adapter = adpKabKota
+    }
+}
